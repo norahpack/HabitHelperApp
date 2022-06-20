e4c184acf681330705827e1b45b63a4b6b98c314
norahpack <norahpack14@gmail.com>
2022-06-20 16:21:12 -0700

    FragmentView set up

--- app/src/main/java/com/example/habithelper/MainActivity.java
+++ app/src/main/java/com/example/habithelper/MainActivity.java
@@ -1,34 +1,74 @@
 package com.example.habithelper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.habithelper.fragments.HomeFragment;
+import com.example.habithelper.fragments.ListFragment;
+import com.example.habithelper.fragments.MoodFragment;
+import com.example.habithelper.fragments.ProfileFragment;
+import com.example.habithelper.fragments.TrackFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnLogout;
+    public static MainActivity self;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    protected BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        self=this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLogout=findViewById(R.id.btnLogout);
+        bottomNavigation=findViewById(R.id.bottomNavigation);
 
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                ParseUser.logOut();
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                Intent i = new Intent (MainActivity.this, LoginActivity.class);
-                startActivity(i);
-
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.itemProfile:
+                        fragment = new ProfileFragment();
+                        break;
+                    case R.id.itemTrack:
+                        fragment = new TrackFragment();
+                        break;
+                    case R.id.itemHome:
+                        //change this
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.itemMood:
+                        //change this
+                        fragment = new MoodFragment();
+                        break;
+                    case R.id.itemList:
+                        //change this
+                        fragment = new ListFragment();
+                        break;
+                    default:
+                        fragment = new HomeFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
             }
         });
+
+        // sets the default fragment to be the home fragment
+        bottomNavigation.setSelectedItemId(R.id.itemHome);
+
     }
 }
---
+++ app/src/main/java/com/example/habithelper/fragments/HomeFragment.java
@@ -0,0 +1,56 @@
+package com.example.habithelper.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.habithelper.LoginActivity;
+import com.example.habithelper.MainActivity;
+import com.example.habithelper.R;
+import com.parse.ParseUser;
+
+public class HomeFragment extends Fragment {
+
+    Button btnLogout;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        btnLogout=view.findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                Intent i = new Intent (getContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
