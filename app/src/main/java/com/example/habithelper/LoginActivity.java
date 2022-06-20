203b7f7272106ce678f0c601f99b488379097597
norahpack <norahpack14@gmail.com>
2022-06-20 12:04:52 -0700

    downloading icons

--- app/src/main/java/com/example/habithelper/LoginActivity.java
+++ app/src/main/java/com/example/habithelper/LoginActivity.java
@@ -7,11 +7,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import java.sql.SQLOutput;
 
 public class LoginActivity extends AppCompatActivity {
 
     public static final String TAG = "LoginActivity";
     private Button btnSignup;
+    private Button btnLogin;
+    private EditText etUsername;
+    private EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,9 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btnSignup=findViewById(R.id.btnSignup);
+        btnLogin=findViewById(R.id.btnLogin);
+        etUsername=findViewById(R.id.etUsername);
+        etPassword=findViewById(R.id.etPassword);
 
         btnSignup.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -29,5 +43,40 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick login button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+
+
+    }
+
+    private void loginUser(String username, String password){
+        System.out.println("here");
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                System.out.println("here2");
+                if (e != null){
+                    System.out.println("error");
+                    Toast.makeText(LoginActivity.this, "Issue with login!", Toast.LENGTH_SHORT);
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Logged in!", Toast.LENGTH_SHORT);
+            }
+        });
+    }
+
+    private void goMainActivity(){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/example/habithelper/SignupActivity.java
+++ app/src/main/java/com/example/habithelper/SignupActivity.java
@@ -2,13 +2,85 @@ package com.example.habithelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class SignupActivity extends AppCompatActivity {
+
+    public static final String TAG = "SignupActivity";
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        etUsername=findViewById(R.id.etUsername);
+        etPassword=findViewById(R.id.etPassword);
+        btnCreate=findViewById(R.id.btnCreate);
+
+        btnCreate.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                if(username.isEmpty()){
+                    Toast.makeText(SignupActivity.this, "Username cannot be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.isEmpty()){
+                    Toast.makeText(SignupActivity.this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                try {
+                    newUser(username, password);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                ParseUser.logInInBackground(username, password, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (e != null){
+                            Toast.makeText(SignupActivity.this, "Issue with login!", Toast.LENGTH_SHORT);
+                            return;
+                        }
+                        Intent i = new Intent(SignupActivity.this, SetupAccount.class);
+                        startActivity(i);
+                        finish();
+                    }
+                });
+            }
+        });
+    }
+
+    private void newUser(String username, String password) throws ParseException {
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.signUp();
+
+        user.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null){
+                    Toast.makeText(SignupActivity.this, "Error creating account", Toast.LENGTH_SHORT).show();
+                }
+                etUsername.setText("");
+                etPassword.setText("");
+            }
+        });
     }
 }
