a1a422954ca0705d3dd8b3747f793ee574a69a11
norahpack <norahpack14@gmail.com>
2022-06-22 19:11:05 -0700

    splash screen is animated!

--- app/src/main/java/com/example/habithelper/SplashScreenActivity.java
+++ app/src/main/java/com/example/habithelper/SplashScreenActivity.java
@@ -3,15 +3,25 @@ package com.example.habithelper;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
 
 public class SplashScreenActivity extends AppCompatActivity {
     Handler handler;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        imageView = findViewById(R.id.imageview);
+
+        // Adding the gif here using glide library
+        Glide.with(this).load(R.drawable.main_screen_new).into(imageView);
+
+
         handler = new Handler();
         handler.postDelayed(new Runnable(){
             @Override
@@ -19,6 +29,6 @@ public class SplashScreenActivity extends AppCompatActivity {
                 startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
                 finish();
             }
-        }, 2000);
+        }, 5000);
     }
 }
